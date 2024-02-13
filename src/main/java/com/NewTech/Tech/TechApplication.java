c7b1d5317362c7b32a93e64bb37556465d2300fe
unknown <karim011236864@gmail.com>
2024-02-14 00:28:41 +0200

    this is the first commit.

---
+++ src/main/java/com/NewTech/Tech/TechApplication.java
@@ -0,0 +1,13 @@
+package com.NewTech.Tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechApplication.class, args);
+	}
+
+}
